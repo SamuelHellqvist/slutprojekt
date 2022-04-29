2df086e619427ef41882308ce93764b87a2160b2
SamuelHellqvist <samuel@hellqvist.nu>
2022-04-29 13:37:09 +0200

    its the dream

--- index.php
+++ index.php
@@ -20,14 +20,22 @@
     <title>Shoot the bandits</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<?php
+    $sql="SELECT * FROM tbl_players";
+    $result=mysqli_query($conn,$sql);
+?>
 <body>
-
+    <!--
+    <div class="reklam">
+        <h1>Kajsa letar efter trevliga kompisar att dricka kaffe med</h1>
+        <a href="https://www.dinmamma.se/">Klicka här för att chatta med henne</a>
+        <p>provided by Swedish Fika Kamrater ltd.</p>
+    </div>-->
     <form method="post" action="index.php">
         <input type="text" name="nick" id="nickname"placeholder="nickname">
         <input type="submit" name="btn" value="save" id="submitbtn" onlclick="showstart()">
     </form>
 
-    <button onclick="showstart()">testa</button>
     </div>
     <div id="gameframe">
         <div id="healthBar"></div>
@@ -37,85 +45,85 @@
         <h1 id="indi">ITALY</h1>
         </div>
 
-        <div class="enemy" id="enemy1" onclick="iShoot(this)">
+        <div class="enemy" id="enemy1" id="foes" onclick="iShoot(this)">
             <div class="lefteye" id="eyes"></div>
             <div class="righteye" id="eyes"></div>
-            <div class="hat">
-                <div class="board"></div>
+            <div class="hat" id="hat1">
+                <div class="board" id="board1"></div>
             </div>
-            <div class="jacket" id="jacket">
+            <div class="jacket" id="jacket1">
                 <div class="zipper"></div>
             </div>
             <div class="shoes"></div>
-            <div class="gun" id="guns">
+            <div class="gun" id="guns1">
                 <div class="line"></div>
                 <div class="pipe" id="pipes"></div>
             </div>
         </div>
 
-        <div class="enemy" id="enemy2" onclick="iShoot(this)">
-        <div class="lefteye" id="eyes"></div>
-            <div class="righteye" id="eyes"></div>
-            <div class="hat">
-                <div class="board">
-                </div>
-            </div>
-            <div class="jacket" id="jacket">
-                <div class="zipper"></div>
-            </div>
-            <div class="shoes"></div>
-            <div class="gun" id="guns">
-                <div class="line"></div>
-                <div class="pipe" id="pipes"></div>
-            </div>
-        </div>
-
-        <div class="enemy" id="enemy3" onclick="iShoot(this)">
+        <div class="enemy" id="enemy2" id="foes" onclick="iShoot(this)">
             <div class="lefteye" id="eyes"></div>
             <div class="righteye" id="eyes"></div>
-            <div class="hat">
-                <div class="board">
+            <div class="hat" id="hat2">
+                <div class="board" id="board2">
                 </div>
             </div>
-            <div class="jacket" id="jacket">
+            <div class="jacket" id="jacket2">
                 <div class="zipper"></div>
             </div>
             <div class="shoes"></div>
-            <div class="gun" id="guns">
+            <div class="gun" id="guns2">
                 <div class="line"></div>
                 <div class="pipe" id="pipes"></div>
             </div>
         </div>
 
-        <div class="enemy" id="enemy4" onclick="iShoot(this)">
-        <div class="lefteye" id="eyes"></div>
+        <div class="enemy" id="enemy3" id="foes" onclick="iShoot(this)">
+            <div class="lefteye" id="eyes"></div>
             <div class="righteye" id="eyes"></div>
-            <div class="hat">
-                <div class="board">
+            <div class="hat" id="hat3">
+                <div class="board" id="board3">
                 </div>
             </div>
-            <div class="jacket" id="jacket">
+            <div class="jacket" id="jacket3">
                 <div class="zipper"></div>
             </div>
             <div class="shoes"></div>
-            <div class="gun" id="guns">
+            <div class="gun" id="guns3">
                 <div class="line"></div>
                 <div class="pipe" id="pipes"></div>
             </div>
         </div>
 
-        <div class="enemy" id="enemy5" onclick="iShoot(this)">
+        <div class="enemy" id="enemy4" id="foes" onclick="iShoot(this)">
         <div class="lefteye" id="eyes"></div>
             <div class="righteye" id="eyes"></div>
-            <div class="hat">
-                <div class="board">
+            <div class="hat" id="hat4">
+                <div class="board" id="board4">
                 </div>
             </div>
-            <div class="jacket" id="jacket">
+            <div class="jacket" id="jacket4">
                 <div class="zipper"></div>
             </div>
             <div class="shoes"></div>
-            <div class="gun" id="guns">
+            <div class="gun" id="guns4">
+                <div class="line"></div>
+                <div class="pipe" id="pipes"></div>
+            </div>
+        </div>
+
+        <div class="enemy" id="enemy5" id="foes" onclick="iShoot(this)">
+        <div class="lefteye" id="eyes"></div>
+            <div class="righteye" id="eyes"></div>
+            <div class="hat" id="hat5">
+                <div class="board" id="board5">
+                </div>
+            </div>
+            <div class="jacket" id="jacket5">
+                <div class="zipper"></div>
+            </div>
+            <div class="shoes"></div>
+            <div class="gun" id="guns5" class="thisclass">
                 <div class="line"></div>
                 <div class="pipe" id="pipes"></div>
             </div>
@@ -126,9 +134,22 @@
     <button onclick="randomEnemyAttacks(), showstart()" id="startbtn" class="jump">START GAME</button>
     <button id="choosemapbtn" onclick="x()">CHANGE LOCATION</button>
     <button id="choosemapbtn2" onclick="y()">CHANGE LOCATION</button>
+
     <div id="highscores">
         <div id="header"><h1>Leaderboard</h1></div>
+        <div id="nickholder">
+    
+        <div class="list">
+         <?php while($row=mysqli_fetch_assoc($result)){ ?>
+            <div>
+                <h1><span><?=$row['tag']?></span></h1>
+            </div>
+        <?php } ?>
+        </div>
+        
+        </div>
     </div>
+
     <script src='script.js'></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function test(){
-    console.log("Never gonna give you up")
-    console.log("Never gonna let you down")
-    console.log("Never gonna run around and desert you")
-    console.log("Never gonna make you cry")
-    console.log("Never gonna say goodbye")
-    console.log("Never gonna tell a lie and hurt you")
+    console.log("Never gonna give you up");
+    console.log("Never gonna let you down");
+    console.log("Never gonna run around and desert you");
+    console.log("Never gonna make you cry");
+    console.log("Never gonna say goodbye");
+    console.log("Never gonna tell a lie and hurt you");
+    console.log(enemy);
 }
 var enemy = document.querySelectorAll(".enemy");
 
@@ -75,11 +76,12 @@ function randomEnemyAttacks() {
 
 }
 
-function z(){
-    enemy.style.left = "-100%";
-}
-
 var hush = 1;
+
+function aliens(enemy){
+    enemy.classList.add("green");
+    console.log("hello world!")
+}
 
 function showstart(){
     console.log("Testing");
@@ -101,7 +103,31 @@ function showstart(){
     }
     if (hush == 2){
         gameframe.style.backgroundImage= "url('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Kibo_PM_interior.jpg/1200px-Kibo_PM_interior.jpg')";
-        z();
+        enemy1.style.backgroundColor = "#ccff8c";
+        enemy2.style.backgroundColor = "#ccff8c";
+        enemy3.style.backgroundColor = "#ccff8c";
+        enemy4.style.backgroundColor = "#ccff8c";
+        enemy5.style.backgroundColor = "#ccff8c";
+        hat1.style.backgroundColor = "white";
+        hat2.style.backgroundColor = "white";
+        hat3.style.backgroundColor = "white";
+        hat4.style.backgroundColor = "white";
+        hat5.style.backgroundColor = "white";
+        jacket1.style.backgroundColor = "#ba9ce4";
+        jacket2.style.backgroundColor = "#ba9ce4";
+        jacket3.style.backgroundColor = "#ba9ce4";
+        jacket4.style.backgroundColor = "#ba9ce4";
+        jacket5.style.backgroundColor = "#ba9ce4";
+        guns1.style.backgroundColor = "silver";
+        guns2.style.backgroundColor = "silver";
+        guns3.style.backgroundColor = "silver";
+        guns4.style.backgroundColor = "silver";
+        guns5.style.backgroundColor = "silver";
+        board1.style.backgroundColor = "silver";
+        board2.style.backgroundColor = "silver";
+        board3.style.backgroundColor = "silver";
+        board4.style.backgroundColor = "silver";
+        board5.style.backgroundColor = "silver";
     }
 }
 
@@ -138,6 +164,24 @@ var enemy2 = document.querySelector("#enemy2");
 var enemy3 = document.querySelector("#enemy3");
 var enemy4 = document.querySelector("#enemy4");
 var enemy5 = document.querySelector("#enemy5");
+var hat1 = document.querySelector("#hat1");
+var hat2 = document.querySelector("#hat2");
+var hat3 = document.querySelector("#hat3");
+var hat4 = document.querySelector("#hat4");
+var hat5 = document.querySelector("#hat5");
+var jacket1 = document.querySelector("#jacket1");
+var jacket2 = document.querySelector("#jacket2");
+var jacket3 = document.querySelector("#jacket3");
+var jacket4 = document.querySelector("#jacket4");
+var jacket5 = document.querySelector("#jacket5");
+var guns1 = document.querySelector("#guns1");
+var guns2 = document.querySelector("#guns2");
+var guns3 = document.querySelector("#guns3");
+var guns4 = document.querySelector("#guns4");
+var guns5 = document.querySelector("#guns5");
+var board1 = document.querySelector("#board1");
+var board2 = document.querySelector("#board2");
+var board3 = document.querySelector("#board3");
+var board4 = document.querySelector("#board4");
+var board5 = document.querySelector("#board5");
 test();
-
-z();
